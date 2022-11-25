6502568c777fe0e7493644590ec46c8375eef72b
Zhayu517 <zhangzy517@gmail.com>
2022-11-25 14:55:13 +0000

    Apply Title component

--- pages/lifes.tsx
+++ pages/lifes.tsx
@@ -1,23 +1,16 @@
-import Head from 'next/head'
 import Header from '../components/Header'
 import styles from '../styles/Lifes.module.css'
 import Footer from '../components/Footer'
+import Title from '../components/Title'
 
 function Lifes({}) {
     return (
       <div className={styles.container}>
-            <Head>
-                <title>Zhaoyu Zhang</title>
-                <meta name="description" content="Zhaoyu Zhang's personal website" />
-                <link rel="icon" href="/Images/me.jpg" />
-            </Head>
-
+            <Title />
             <Header />
-
             <main className={styles.main}>
                 <h1>This Site is Under Development</h1>
             </main>
-
             <Footer />
         </div>
     );
